702b8a453dfea32904ad535d47fb8d0e1169bb81
unknown <jdiegov15@gmail.com>
2022-05-28 20:22:17 -0500

    Se crea asignar valor

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,4 @@
-let cadena = "A0000B0000C0000T0000S0000"; // 1001000110011010111101110111
+let cadena = "A0000B0000C0000T0000S000100100"; // 0110011010111101110111
 let posicion = 0; // Posición de la cinta
 let cinta = cadena.split("");
 let estadoActual = 'A'; // Valor que se toma en el estado actual
@@ -14,6 +14,8 @@ document.addEventListener('DOMContentLoaded', event => {
 
     // Cargar a la IG la cadena     
     leerInstrucciones();
+
+    console.log(cinta);
 })
 
 /**
@@ -38,8 +40,9 @@ let leerInstrucciones = () => {
                         posicion++;
                         switch (cinta[posicion]) {
                             case '0':
-                                console.log("Ejecutar Asignar Valor\n");
                                 posicion++;
+                                estadoActual = cinta[posicion]
+                                identificarVariable();
                                 break;
                             case '1':
                                 console.log("Ejecutar Asignar Variable\n");
@@ -129,3 +132,140 @@ let pasarbytes = () => {
 
 };
 
+
+
+
+let identificarVariable = () => {
+    let bandera = true
+    while (bandera && posicion < cinta.length) {
+        switch (estadoActual) {
+            case '0':
+                posicion++
+                switch (cinta[posicion]) {
+                    case '0':
+                        posicion++;
+
+                        asignarValor("A");
+                        bandera = false
+                        break;
+                    case '1':
+                        posicion++;
+                        asignarValor("B")
+                        bandera = false
+
+                        break;
+                    default:
+                        estadoActual == '-1'
+                        bandera = false
+
+                        break;
+                }
+                break;
+            case '1':
+                posicion++
+                switch (cinta[posicion]) {
+                    case '0':
+                        posicion++;
+                        asignarValor("C")
+                        bandera = false
+
+                        break;
+                    default:
+                        estadoActual == '-1'
+                        bandera = false
+
+                        break;
+                }
+                break;
+            default:
+                console.log("ALgo falla");
+                bandera = false
+
+                break;
+        }
+    }
+}
+
+let asignarValor = (variable) => {
+    let cases // ignorar a la izquierda
+    let esc // ignorar a la derecha
+    let aux
+    let condEscape
+    if (variable === "A") {
+        cases = ["0", "1", "S", "T", "C", "B"]
+        esc = ["0", "1", "S", "T", "C"]
+        aux = 'A'
+        condEscape = 'B'
+    } else if (variable === "B") {
+        cases = ["0", "1", "S", "T", "C",]
+        esc = ["0", "1", "S", "T"]
+        aux = 'B'
+        condEscape = 'C'
+    } else {
+        cases = ["0", "1", "S", "T",]
+        esc = ["0", "1", "S"]
+        aux = 'C'
+        condEscape = 'T'
+    }
+    let bandera = true;
+    while (bandera) {
+        switch (estadoActual) {
+            case '0':
+                cinta[posicion] = 'X'
+                estadoActual = posicion--;
+                // Ciclo para ignorar a la izquierda y ubicarnos sobre la variable
+                while (cases.includes(cinta[posicion]) && posicion >= 0) {
+                    posicion--;
+                }
+
+                if (cinta[posicion] === aux) {
+                    posicion++;
+                }
+                cinta[posicion] = '0'
+                posicion++;
+                if (cinta[posicion] != condEscape) {
+                    cinta[posicion] = 'X'
+                    while (cases.includes(cinta[posicion]) && posicion < cinta.length) {
+                        posicion++;
+                    }
+                    cinta[posicion] = '0';
+                    posicion++;
+                } else {
+                    while (esc.includes(cinta[posicion]) && posicion <= cinta.length) {
+                        posicion++;
+                    }
+                    bandera = false
+                }
+                break;
+            case '1':
+                cinta[posicion] = 'Y'
+                estadoActual = posicion--;
+                // Ciclo para ignorar a la izquierda y ubicarnos sobre la variable
+                while (cases.includes(cinta[posicion]) && posicion >= 0) {
+                    posicion--;
+                }
+
+                if (cinta[posicion] === aux) {
+                    posicion++;
+                }
+                cinta[posicion] = '1'
+                posicion++;
+                if (cinta[posicion] != condEscape) {
+                    cinta[posicion] = 'Y'
+                    while (cases.includes(cinta[posicion]) && posicion < cinta.length) {
+                        posicion++;
+                    }
+                    cinta[posicion] = '1';
+                    posicion++;
+                } else {
+                    while (esc.includes(cinta[posicion]) && posicion <= cinta.length) {
+                        posicion++;
+                    }
+                    bandera = false
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
